bd949d5a163d58c96eeab67db8a4ca30484cefc0
Tanush Gupta <72255106+Tanush-gupta@users.noreply.github.com>
2024-09-13 23:25:46 +0530

    fixed build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
 import Index from './navigation/index.js';
-import './fireabaseConfig.js';
 
 export default function App() {
   return <Index />;
